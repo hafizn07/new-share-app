c48f54114a9dd496a1dd4f8e5a0e740a7bb9c711
hafizmp <hafiznjh007@gmail.com>
2022-10-07 23:27:48 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose')
+const File = require('./models/File')
+const bcrypt = require('bcrypt')
+mongoose.connect(process.env.DATABASE_URL)
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.route("/file/:id").get(handleDownload).post(handleDownload)
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id)
+
+  if (file.password != null) {
+    if(req.body.password == null){
+      res.render("password")
+      return
+    }
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true })
+      return
+    }
+  }
+
+  file.downloadCount++
+  await file.save()
+  console.log(file.downloadCount);
+
+  res.download(file.path, file.originalName)
+}
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+const bcrypt = require('bcrypt')
+const File = require('../models/File');
+
+
+const upload = multer({ dest: "uploads" })
+var app = express();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'New Share | Share securely' });
+});
+
+router.post('/upload', upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  }
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10)
+  }
+
+  const file = await File.create(fileData)
+  res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}` , title: 'New Share | Share securely'} )
+
+})
+
+
+
+module.exports = router;
